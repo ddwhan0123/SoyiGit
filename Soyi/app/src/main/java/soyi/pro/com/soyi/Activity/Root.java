862abc43b62d0b3a4b0649f72338651d7fadc38e
王亟亟 <526357367@qq.com>
2015-12-01 11:03:38 +0800

    更新部分代碼

--- Soyi/app/src/main/java/soyi/pro/com/soyi/Activity/Root.java
+++ Soyi/app/src/main/java/soyi/pro/com/soyi/Activity/Root.java
@@ -11,7 +11,8 @@ import soyi.pro.com.soyi.Tools.DialogUtils;
 import soyi.pro.com.soyi.Tools.ToastUtils;
 
 public class Root extends Activity implements View.OnClickListener{
-    Button showTitleDialog,showTitleAndMsgDialog;
+    Button showTitleDialog,showTitleAndMsgDialog,showErrorDialog,showWarningDialog,
+            showSuccessDialog,showCustomIconDialog,showWarningListenerDialog,showWarningAllDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +25,23 @@ public class Root extends Activity implements View.OnClickListener{
     public void findId(){
         showTitleDialog=(Button)findViewById(R.id.showTitleDialog);
         showTitleAndMsgDialog=(Button)findViewById(R.id.showTitleAndMsgDialog);
+        showErrorDialog=(Button)findViewById(R.id.showErrorDialog);
+        showWarningDialog=(Button)findViewById(R.id.showWarningDialog);
+        showSuccessDialog=(Button)findViewById(R.id.showSuccessDialog);
+        showCustomIconDialog=(Button)findViewById(R.id.showCustomIconDialog);
+        showWarningListenerDialog=(Button)findViewById(R.id.showWarningListenerDialog);
+        showWarningAllDialog=(Button)findViewById(R.id.showWarningAllDialog);
     }
 
     public  void setClickener(){
         showTitleDialog.setOnClickListener(this);
         showTitleAndMsgDialog.setOnClickListener(this);
+        showErrorDialog.setOnClickListener(this);
+        showWarningDialog.setOnClickListener(this);
+        showSuccessDialog.setOnClickListener(this);
+        showCustomIconDialog.setOnClickListener(this);
+        showWarningListenerDialog.setOnClickListener(this);
+        showWarningAllDialog.setOnClickListener(this);
     }
 
     protected void showToast(Context context,String msg,boolean isLong){
@@ -39,10 +52,28 @@ public class Root extends Activity implements View.OnClickListener{
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.showTitleDialog:
-                DialogUtils.showTitleDialog(this,"標題Dialog");
+                DialogUtils.showTitleDialog(this, "標題Dialog");
                 break;
             case R.id.showTitleAndMsgDialog:
-                DialogUtils.showTitleAndMsgDialog(this,"標題內容","具體的提示信息");
+                DialogUtils.showTitleAndMsgDialog(this, "標題內容", "具體的提示信息");
+                break;
+            case R.id.showErrorDialog:
+                DialogUtils.showErrorDialog(this,"錯誤的標題","錯誤的文字內容");
+                break;
+            case R.id.showWarningDialog:
+                DialogUtils.showWarningDialog(this,"提示的標題","提示的具體內容","確認按鈕");
+                break;
+            case  R.id.showSuccessDialog:
+                DialogUtils.showSuccessDialog(this,"標題","文字內容");
+                break;
+            case R.id.showCustomIconDialog:
+                DialogUtils.showCustomIconDialog(this,"自定義標題","自定義文字內容，字數要多",R.drawable.icon_zbx);
+                break;
+            case R.id.showWarningListenerDialog:
+                DialogUtils.showWarningListenerDialog(this,"標題","提示內容","按鈕文字");
+                break;
+            case R.id.showWarningAllDialog:
+                DialogUtils.showWarningAllDialog(this,"標題","對話框中的內容","取消","確認");
                 break;
         }
     }
--- Soyi/app/src/main/java/soyi/pro/com/soyi/Tools/DialogUtils.java
+++ Soyi/app/src/main/java/soyi/pro/com/soyi/Tools/DialogUtils.java
@@ -8,7 +8,8 @@ import cn.pedant.SweetAlert.SweetAlertDialog;
  * Created by Ezreal on 2015/11/30.
  */
 public class DialogUtils {
-    /*
+
+    /*標題對話框
     * @param Context context:上下文對象
     * @param String msg:標題的文字
     * */
@@ -18,15 +19,118 @@ public class DialogUtils {
                 .show();
     }
 
-    /*
+    /*標題+文字對話框
     * @param Context context:上下文對象
     * @param String title:標題
     * @param String msg: 具體文字內容
     * */
-    public static void showTitleAndMsgDialog(Context context,String title,String msg){
+    public static void showTitleAndMsgDialog(Context context, String title, String msg) {
         new SweetAlertDialog(context)
                 .setTitleText(title)
                 .setContentText(msg)
+                .show();
+    }
+
+    /*錯誤提示對話框
+    * @param Context context:上下文對象
+    * @param String title:標題
+    * @param String msg: 具體文字內容
+    * */
+    public static void showErrorDialog(Context context, String eTitle, String msg) {
+        new SweetAlertDialog(context, SweetAlertDialog.ERROR_TYPE)
+                .setTitleText(eTitle)
+                .setContentText(msg)
+                .show();
+    }
+
+    /*警告對話框
+    * @param Context context:上下文對象
+    * @param String eTitle: 標題
+    * @param String msg:具體的文字內容
+    * @param String confimText:按鈕的文字
+    * */
+    public static void showWarningDialog(Context context, String eTitle, String msg, String confirmText) {
+        new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText(eTitle)
+                .setContentText(msg)
+                .setConfirmText(confirmText)
+                .show();
+    }
+
+    /*成功對話框
+    * @param Context context:上下文對象
+    * @param String eTitle: 標題
+    * @param String msg:具體的文字內容
+    * */
+    public static void showSuccessDialog(Context context, String title, String msg) {
+        new SweetAlertDialog(context, SweetAlertDialog.SUCCESS_TYPE)
+                .setTitleText(title)
+                .setContentText(msg)
+                .show();
+    }
+
+    /*自定義icon對話框
+    * @param Context context:上下文對象
+    * @param String eTitle: 標題
+    * @param String msg:具體的文字內容
+    * @param int icon:圖片素材
+    * */
+    public  static void showCustomIconDialog(Context context,String title,String msg,int icon){
+        new SweetAlertDialog(context, SweetAlertDialog.CUSTOM_IMAGE_TYPE)
+                .setTitleText(title)
+                .setContentText(msg)
+                .setCustomImage(icon)
+                .show();
+    }
+
+    /*綁定按鈕事件對話框
+    * @param Context context:上下文對象
+    * @param String eTitle: 標題
+    * @param String msg:具體的文字內容
+    * @param String confirmText:按鈕的文字
+    * setConfirmClickListener: 監聽事件
+    * */
+    public static void showWarningListenerDialog(final Context context,String title,String msg,String confirmText){
+        new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText(title)
+                .setContentText(msg)
+                .setConfirmText(confirmText)
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        ToastUtils.show(context, "吐出了一句話", false);
+                        sDialog.dismissWithAnimation();
+                    }
+                })
+                .show();
+    }
+       /*綁定複雜按鈕事件對話框
+       * @param Context context:上下文對象
+       * @param String eTitle: 標題
+       * @param String msg:具體的文字內容
+       * @param String confirmText:按鈕的文字
+       * setConfirmClickListener: 確認監聽事件
+       * setCancelClickListener: 取消監聽事件
+       * */
+    public static  void showWarningAllDialog(final Context context, final String title,String msg,String cancelText,String confirmText){
+        new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText(title)
+                .setContentText(msg)
+                .setCancelText(cancelText)
+                .setConfirmText(confirmText)
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        sDialog.dismissWithAnimation();
+                    }
+                })
+                .showCancelButton(true)
+                .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        sDialog.cancel();
+                    }
+                })
                 .show();
     }
 
